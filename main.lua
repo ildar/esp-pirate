358049865825360f0918d960d3cd0f7f0ee7b0c9
Ildar Mulyukov <ildar@altlinux.ru>
2019-07-23 07:07:17 +0000

    refactor menu organization

--- main.lua
+++ main.lua
@@ -1,25 +1,28 @@
 local RL = require 'readline'
 
 -- load menus and sub-menus
-local cli_root = require "src.cli_root"
 local backend_nodemcu = require "src.backend_nodemcu"
+local menus = {
+  root = require "src.cli_root",
+}
+
 
 -- TODO: maybe ignoredups=true
 RL.set_options{ keeplines=1000, histfile='rl_history' }
 
 local cmd
-local current_cli = cli_root
-RL.set_complete_list(current_cli._reserved_words)
+local current_cli = menus.root
 
 -- prepare
 app_context = {
   backend = backend_nodemcu,
   switch_menu = function(name)
-    -- current_cli <- name ?
+    current_cli = menus[name]
     RL.set_complete_list(current_cli._reserved_words)
   end,
 }
 
+app_context.switch_menu("root")
 while true do
   cmd = RL.readline(current_cli._prompt)
   if not cmd or cmd == "quit" then break end
